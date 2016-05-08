e5e45fbb0c019dea80a90a2d88730839d8ee3628
Cody Barber <cody.n.barber@gmail.com>
2016-05-08 18:12:02 -0400

    First version of my number guessing game

---
+++ guessnumber.js
@@ -0,0 +1,21 @@
+debugger;
+
+var num = Math.random() * 100;
+var number = Math.floor(num);
+var i = 0;
+var guess = Number(prompt("I'm thinking of a number between 1 and 100. Guess which one??"));
+
+
+for (i = 1; i <= 5; i++) {
+  if (i === 5) {
+    alert("You took too many guesses! Please refresh and try again!")
+    break;
+  } if (guess === number) {
+    alert("You are correct!! It only took you " + i + " guesses!");
+    break;
+  } else if (guess > number) {
+    guess = Number(prompt("Your guess of " + guess + " was too high! Try again!"));
+  } else if (guess < number) {
+    guess = Number(prompt("Your guess of " + guess + " was too low! Try again!"));
+  }
+}
