baa1ca394ed1cbdb1f3225399c5d1c58a478be59
Cody Barber <cody.n.barber@gmail.com>
2016-05-08 18:18:38 -0400

    Changed number of guesses from 5 to 10.

--- guessnumber.js
+++ guessnumber.js
@@ -6,8 +6,8 @@ var i = 0;
 var guess = Number(prompt("I'm thinking of a number between 1 and 100. Can you guess which one??"));
 
 
-for (i = 1; i <= 5; i++) {
-  if (i === 5) {
+for (i = 1; i <= 10; i++) {
+  if (i === 10) {
     alert("You took too many guesses! Please refresh and try again!")
     break;
   } if (guess === number) {
